b6e31f2468271603bd00e8931b779ab94c094a84
EtienneLa <etienne.langensiepen@gmx.de>
2018-08-05 16:27:30 +0200

    getting started (hello world, elif)

---
+++ HackerrankProblems/Hello, World!.py
@@ -0,0 +1,6 @@
+print("Hello, World!")
+
+string_one = "Hello"
+string_two = ", World!"
+
+print(string_one + string_two)
---
+++ HackerrankProblems/If-Else.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+if n % 2 > 0:
+    print("weird")
+elif n > 20 or n < 6:
+    print("not weird")
+else:
+    print("weird")
