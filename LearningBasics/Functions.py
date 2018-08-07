212b6de0eefe0e7a32e5a478bda155fc7213e2ce
EtienneLa <etienne.langensiepen@gmx.de>
2018-08-07 18:22:48 +0200

    simple functions

---
+++ LearningBasics/Functions.py
@@ -0,0 +1,14 @@
+def just_print():
+    print("It does what it has to do.")
+
+
+def bitcoin_to_usd(btc):
+    amount = btc * 527
+    print(amount)
+
+
+just_print()
+bitcoin_to_usd(12)
+bitcoin_to_usd(1)
+bitcoin_to_usd(3.75)
+bitcoin_to_usd(2.5)
