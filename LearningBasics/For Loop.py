b219b81453ef02c67466f66ca077c7c49e2e5983
EtienneLa <etienne.langensiepen@gmx.de>
2018-08-07 17:56:52 +0200

    if elif else / for loops

---
+++ LearningBasics/For Loop.py
@@ -0,0 +1,7 @@
+food = ["bacon", "tuna", "ham", "banana", "beef"]
+
+for f in food[:4]:
+    print(f)
+    print(len(f))
+    if f == "ham":
+        print("I love ham!")
---
+++ LearningBasics/If Elif Else.py
@@ -0,0 +1,17 @@
+age = 25
+
+if age < 16:
+    print("No beer for you.")
+else:
+    print("Have a beer, my friend!")
+
+
+name = "Lucy"
+
+if name == "Bob":
+    print("Bob, mh? Great name!")
+elif name == "Lucy":
+    print("Hey Lucy, nice to meet you!")
+else:
+    print("Okay, whatever!")
+
