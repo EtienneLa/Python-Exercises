e703d8a654affb8a5518128ffb797a8623d85f72
EtienneLa <etienne.langensiepen@gmx.de>
2018-08-07 17:18:18 +0200

    refreshing basics for Python

---
+++ LearningBasics/Numbers and Strings.py
@@ -0,0 +1,28 @@
+a = 3 + 4
+b = 3 * 20
+c = 12 / 4
+d = 8 + 2 * 10
+e = 18 // 4
+f = 18 / 4
+g = 24 - 12
+h = (8 + 2) * 10
+
+print(a, b, c, d, e, f, g, h)
+
+i = 4 ** 3
+
+print(i)
+
+# ------------------------------------------
+
+print("I can't do it.")
+print('"The man in black fled across the desert, and the gunslinger followed." - The Dark Tower')
+print("Great\nfun!")
+print(r"Great fun!")
+
+firstName = "Max "
+lastName = "Mustermann"
+firstName += "Peter "
+fullName = firstName + lastName
+
+print(fullName)
