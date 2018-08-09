23e17086687f426efeb489bec1bbbab626fda6d6
EtienneLa <etienne.langensiepen@gmx.de>
2018-08-09 23:11:07 +0200

    Modules, random

---
+++ LearningBasics/ModulesInclusion.py
@@ -0,0 +1,3 @@
+def do_something(n, m):
+    sum = n+m
+    print(sum)
---
+++ LearningBasics/ModulesMain.py
@@ -0,0 +1,9 @@
+import ModulesInclusion
+import random
+
+x = random.randrange(1, 100)
+y = random.randrange(1, 100)
+
+ModulesInclusion.do_something(x, y)
+
+
