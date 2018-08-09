558b55855805243b7d51ffc5520726dac9130bb8
EtienneLa <etienne.langensiepen@gmx.de>
2018-08-09 22:57:45 +0200

    sets

---
+++ LearningBasics/Sets.py
@@ -0,0 +1,9 @@
+# sets contain every item just once
+groceries = {"cereal", "milk", "cornflakes", "beer", "duct tape", "lotion", "beer"}
+print(groceries)
+
+if "milk" in groceries:
+    print("You already have milk added.")
+else:
+    print("You need to add milk, bud.")
+
