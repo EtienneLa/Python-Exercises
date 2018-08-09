7614832dd8de002740a2b0b0150220fdd8dd707f
EtienneLa <etienne.langensiepen@gmx.de>
2018-08-09 23:03:31 +0200

    dictionary

---
+++ LearningBasics/Dictionary.py
@@ -0,0 +1,8 @@
+classmates = {"Tony":"cool guy, black hair", "Emma":"cool girl, blond hair", "Max":"random guy, no hair"}
+
+print(classmates)
+print(classmates["Tony"])
+
+for k, v in classmates.items():
+    print(k, ": ", v)
+
