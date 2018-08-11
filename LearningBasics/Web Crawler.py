c7cad74b08f81df20f0b9475cafdf2d49048dcf6
EtienneLa <etienne.langensiepen@gmx.de>
2018-08-12 01:47:09 +0200

    clean up

--- LearningBasics/Web Crawler.py
+++ LearningBasics/Web Crawler.py
@@ -8,7 +8,7 @@ def book_spider(max_pages):
         url = "http://books.toscrape.com/catalogue/category/books_1/page-" + str(page) + ".html"
         source_code = requests.get(url)
         plain_text = source_code.text
-        soup = BeautifulSoup(plain_text, features="html.parser")
+        soup = BeautifulSoup(plain_text, "html.parser")
         for link in soup.find_all("a"):
             title = str(link.get("title"))
             if title != "None":
@@ -16,6 +16,4 @@ def book_spider(max_pages):
         page += 1
 
 
-book_spider(5)
-
-
+book_spider(1)
