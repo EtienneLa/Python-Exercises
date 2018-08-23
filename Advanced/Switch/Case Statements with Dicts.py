387eab5443004e8ddaa2869349d0ccebc1496ecb
EtienneLa <etienne.langensiepen@gmx.de>
2018-08-23 16:12:04 +0200

    emulate switch/case statements

---
+++ Advanced/Switch/Case Statements with Dicts.py
@@ -0,0 +1,23 @@
+# standard way of handling if statements
+
+def dispatch_if(operator, x, y):
+    if operator == 'add':
+        return x + y
+    elif operator == 'sub':
+        return x - y
+    elif operator == 'mul':
+        return x * y
+    elif operator == 'div':
+        return x / y
+    return None
+
+
+# with dict and lambda (slower but cleaner)
+
+def dispatch_if_v2(operator, x, y):
+    return {
+        'add': lambda: x + y,
+        'sub': lambda: x - y,
+        'mul': lambda: x * y,
+        'div': lambda: x /y,
+    }.get(operator, lambda: None)()
