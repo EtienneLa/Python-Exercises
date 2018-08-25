a8125e9bde03909cef416a085932b1186ec2d713
EtienneLa <etienne.langensiepen@gmx.de>
2018-08-25 16:52:34 +0200

    dict unpacking

--- Advanced/Argument Unpacking.py
+++ Advanced/Argument Unpacking.py
@@ -9,8 +9,12 @@ list_vec = [1, 0, 1]
 
 print_vector(tuple_vec[0], tuple_vec[1], tuple_vec[2])
 
-print_vector(*tuple_vec)
+print_vector(*list_vec)
 
 gen_expr = (x * x for x in range(3))
-
 print_vector(*gen_expr)
+
+dict_vec = {'x': 1, 'y': 2, 'z': 3}
+
+print_vector(*dict_vec)
+print_vector(**dict_vec)
