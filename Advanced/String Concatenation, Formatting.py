2d9791c6b1c28708b711cebfe5bbbf77efe089f0
EtienneLa <etienne.langensiepen@gmx.de>
2018-08-26 19:36:14 +0200

    String Formatting

--- Advanced/String Concatenation, Formatting.py
+++ Advanced/String Concatenation, Formatting.py
@@ -13,3 +13,10 @@ location_of_files = 'C:\\Users\\Me\\Desktop\\Python'
 file_name = 'example.txt'
 print(location_of_files + "\\" + file_name)
 print(os.path.join(location_of_files, file_name))
+
+
+who = 'Gary'
+how_many = 12
+
+print(who, 'bought', how_many, 'apples today!')
+print('{} bought {} apples today!'.format(who, how_many))
